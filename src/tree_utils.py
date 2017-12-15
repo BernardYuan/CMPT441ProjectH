7b1f04fc642f110b8f73a9472e5489467aa27f19
BernardYuan <bernardy@sfu.ca>
2017-12-14 21:49:08 -0800

    modified tree utils to support Jukes&Cantor model

--- src/tree_utils.py
+++ src/tree_utils.py
@@ -2,17 +2,45 @@ from Bio import SeqIO
 from Bio.Phylo.TreeConstruction import DistanceCalculator
 from Bio import AlignIO
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+from Bio.Phylo.TreeConstruction import DistanceMatrix
 from Bio import Phylo
+import numpy as np
+
+def JukesCantorDistanceMatrix(msa) :
+    names = [seq.id for seq in msa]
+    matrix = []
+
+    rowIdx = 0
+    for row in msa:
+        matrix.append([])
+        for col in msa:
+            if col.id == row.id :
+                matrix[rowIdx].append(0)
+                break
+            else :
+                strLen = len(row.seq)
+                diff = 0
+                for i in range(strLen) :
+                    if row.seq[i] != col.seq[i] :
+                        diff = diff + 1
+                JDdist = -0.75 * np.log(1 - 4./3 * (1.0 * diff / strLen))
+                matrix[rowIdx].append(JDdist)
+        rowIdx = rowIdx + 1
+    return DistanceMatrix(names, matrix)
+
+        
 
 
-def build_tree_NJ(msaFile, msaFileType, distanceModel="identity") :
-    # read the msa and compute pairwise distance
-    msa = AlignIO.read(msaFile, msaFileType)
-    distCalculator = DistanceCalculator(distanceModel)
+def build_tree_NJ(msa, distanceMatrix=None) :
+    if not distanceMatrix :
+        distCalculator = DistanceCalculator("identity")
+        distanceMatrix = distCalculator.get_distance(msa)
 
-    # Construct the tree
-    constructor = DistanceTreeConstructor(distCalculator, 'nj')
-    tree = constructor.build_tree(msa)
+    # Construct the tree with the distance Matrix
+    constructor = DistanceTreeConstructor()
+    tree = constructor.nj(distanceMatrix)
 
+    # Make the tree rooted
+    tree.root_at_midpoint()
     #return newick format
-    return tree.format("newick")
+    return "[&R] " + tree.format("newick").strip()
